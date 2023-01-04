b416a450f0628f2046d601a6239800eb5dddbb44
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2023-01-04 08:43:22 +0100

    Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     PokemonModule,
     AppRoutingModule, // Déclaré en dernier pour avoir les routes qui fonctionnent sans tomber sur la page 404
   ],
